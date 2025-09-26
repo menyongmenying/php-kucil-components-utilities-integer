94723f08c47da0b299806098c61f9a09115ab84c
menyongmenying <menyongmenying.main@gmail.com>
2025-09-26 09:15:29 +0700

    Commit pertama.

---
+++ src/Enums/IntegerRoundOptions.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Kucil\Components\Utilities\Enums;
+
+/**
+ * @author  menyongmenying <menyongmenying.main@email.com>
+ * 
+ * @version 0.0.1
+ * 
+ * 
+ * 
+ */
+enum IntegerRoundOptions
+{
+    case NEAREST;
+    case UP;
+    case Down;
+}
---
+++ src/IntegerUtils.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Kucil\Components\Utilities;
+
+use Kucil\Components\Utilities\Core\IntegerCore;
+
+/**
+ * @author  menyongmenying <menyongmenying.main@email.com>
+ * 
+ * @version 0.0.1
+ * 
+ * 
+ * 
+ */
+class IntegerUtils extends IntegerCore
+{}
